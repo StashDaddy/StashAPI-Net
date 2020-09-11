73d1baaab7209b37dcfaedb6ec0129098c2a60ae
Chris Evans <evans.chris.b@gmail.com>
2020-09-11 10:46:07 -0500

    Updated readme, added usage notes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # StashAPI-NET
 StashAPI for .NET (C# Implementations)
+To use this project, add the project to your existing solution, then add a reference 
+to the shared project from your existing code/application project.
--- STASHAPI/STASHAPI.cs
+++ STASHAPI/STASHAPI.cs
@@ -1,4 +1,12 @@
-﻿using System;                           // Reference: System
+﻿/* This is a Visual Studio Shared Project
+ * To use this project, add the project to your existing solution, then add a reference to the shared
+ * project from your existing code/application project.
+ * Structure:
+ * - Solution
+ * -- Code .net/c#/vb project
+ * -- STASHAPI shared project
+ */
+using System;                           // Reference: System
 using System.Text;                      // Reference: System
 using System.Text.RegularExpressions;   // Reference: System
 using System.Security.Cryptography;     // Reference: System
